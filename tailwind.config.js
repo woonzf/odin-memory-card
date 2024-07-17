e05b36c235fb8ac1c0bf138a2dd1aaf044a03c7e
woonzf <137765412+woonzf@users.noreply.github.com>
2024-07-17 21:22:25 +0800

    feat: add a background image and font chore: add a note for reference

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "luckiest-guy": ["Luckiest Guy"],
+      },
+    },
   },
   plugins: [],
 }
-
