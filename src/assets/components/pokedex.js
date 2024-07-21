f24de1b05497bdee51f395879bf4a7b6e9b3d24f
woonzf <137765412+woonzf@users.noreply.github.com>
2024-07-21 18:07:27 +0800

    fix: pokedex now generate index from 1 to 151, add id to getPokemon

--- src/assets/components/pokedex.js
+++ src/assets/components/pokedex.js
@@ -1,5 +1,5 @@
 // Gen 1 pokemon only
-let pokedexCount = 150;
+let pokedexCount = 152;
 
 export async function generatePokemonList(card) {
   const url = "https://pokeapi.co/api/v2/pokemon/";
@@ -14,6 +14,7 @@ async function getPokemon(url, index) {
   const response = await fetch(url + index);
   const json = await response.json();
   return {
+    id: index,
     name: json.name,
     sprite: json.sprites.front_default,
     cry: json.cries.legacy,
@@ -24,8 +25,9 @@ function generateIndexList(card) {
   let list = [];
 
   for (let i = 0; i < card; i++) {
-    let randomInt = -1;
-    while (list.includes(randomInt) || randomInt === -1)
+    // PokeAPI pokemon id starts from 1;
+    let randomInt = 0;
+    while (list.includes(randomInt) || randomInt === 0)
       randomInt = getRandomInt(pokedexCount);
     list.push(randomInt);
   }
