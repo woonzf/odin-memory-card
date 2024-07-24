b1ce46eca6fb2c340b1cb805cf6614dc2282d324
woonzf <137765412+woonzf@users.noreply.github.com>
2024-07-24 22:11:35 +0800

    feat: add sprites for Gym Leader and Rival

--- src/assets/modules/character.js
+++ src/assets/modules/character.js
@@ -11,8 +11,19 @@ import beauty from "../img/trainer/VSBeauty.png";
 import blackBelt from "../img/trainer/VSBlack_Belt.png";
 import bugCatcher from "../img/trainer/VSBug_Catcher.png";
 import psychic from "../img/trainer/VSPsychic.png";
-import sailor from "../img/trainer/VSSailor.png";
 import swimmer from "../img/trainer/VSSwimmer_M_VI.png";
+import youngster from "../img/trainer/VSYoungster.png";
+
+// Gym Leader
+import blaine from "../img/gym_leader/blaine.png";
+import brock from "../img/gym_leader/brock.png";
+import giovanni from "../img/gym_leader/giovanni.png";
+import koga from "../img/gym_leader/koga.png";
+import misty from "../img/gym_leader/misty.png";
+import sabrina from "../img/gym_leader/sabrina.png";
+
+// Rival
+import blue from "../img/gym_leader/blue.png";
 
 const wildPokemon = [
   { name: "Bulbasaur", sprite: bulbasaur },
@@ -26,11 +37,22 @@ const trainer = [
   { name: "Black Belt", sprite: blackBelt },
   { name: "Bug Catcher", sprite: bugCatcher },
   { name: "Psychic", sprite: psychic },
-  { name: "Sailor", sprite: sailor },
   { name: "Swimmer", sprite: swimmer },
+  { name: "Youngster", sprite: youngster },
 ];
 
-const list = [wildPokemon, trainer];
+const gymLeader = [
+  { name: "Blaine", sprite: blaine },
+  { name: "Brock", sprite: brock },
+  { name: "Giovanni", sprite: giovanni },
+  { name: "Koga", sprite: koga },
+  { name: "Misty", sprite: misty },
+  { name: "Sabrina", sprite: sabrina },
+];
+
+const rival = [{ name: "Blue", sprite: blue }];
+
+const list = [wildPokemon, trainer, gymLeader, rival];
 let currentCharacter = null;
 
 export function setCharacter(index) {
