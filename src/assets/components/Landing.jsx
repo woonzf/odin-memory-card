12885ac13349c6ee1e5d15d206e85915ed5d3359
woonzf <137765412+woonzf@users.noreply.github.com>
2024-07-25 23:11:14 +0800

    feat: update Landing message

--- src/assets/components/Landing.jsx
+++ src/assets/components/Landing.jsx
@@ -3,12 +3,18 @@ function Landing({ onClick }) {
     <button className="h-full w-full flex justify-center" onClick={onClick}>
       <div className="h-full w-full max-w-screen-xl flex flex-col">
         <div className="h-[43%] flex justify-center items-end">
-          <div className="h-[50%] px-4 flex items-center">
+          <div className="h-[50%] px-4 flex flex-col justify-center gap-[5vh]">
             <big>A memory card game inspired by Pokémon...</big>
+            <small>
+              This game is not associated with The Pokémon Company in any way.
+              It is developed solely for the purpose of learning. I do not take
+              credit for any of the names, graphics, sounds and musics used. All
+              credits belong to their rightful owners.
+            </small>
           </div>
         </div>
         <div className="h-[31%] w-full mt-[26vh] flex flex-col justify-center">
-          <div>Press Anywhere</div>
+          <div>Click Anywhere</div>
           <div>To Continue...</div>
         </div>
       </div>
