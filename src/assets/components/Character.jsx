0de4832fa4a98db408ac1570cebfd355ade782c3
woonzf <137765412+woonzf@users.noreply.github.com>
2024-07-26 13:42:29 +0800

    fix: adjust Character sprite position

--- src/assets/components/Character.jsx
+++ src/assets/components/Character.jsx
@@ -2,12 +2,12 @@ function Character({ character, state }) {
   return (
     <div
       className={
-        "h-full w-[120%] xl:w-auto flex absolute top-0 left-0 xl:left-auto xl:right-0 animate-slide-left " +
+        "h-full aspect-[2] flex absolute top-0 right-0 animate-slide-left " +
         (state > 4 ? "opacity-80" : "")
       }
     >
       <img
-        className="h-full object-cover translate-x-[20%] xl:translate-x-0"
+        className="h-full object-cover portrait:translate-x-1/3 portrait:sm:translate-x-[20%]"
         src={character.sprite}
       />
     </div>
--- src/assets/components/Popup.jsx
+++ src/assets/components/Popup.jsx
@@ -12,16 +12,16 @@ function Popup({ state, onReplay, onChangeDifficulty, help, onClose }) {
 
   return (
     <div className="h-full w-full shadow-pokedarkblue flex items-center absolute">
-      <div className="h-[33%] w-full bg-pokeblue bg-opacity-90 border-y-[0.5vh] border-pokedarkblue flex justify-center items-center animate-end">
+      <div className="h-[33%] w-full bg-pokeblue bg-opacity-90 border-y-[0.5vh] border-pokedarkblue flex justify-center items-center animate-end overflow-hidden">
         <div className="h-full w-full max-w-screen-lg portrait:max-w-screen-md">
           {state === 3 && (
-            <div className="h-full p-5 flex flex-col justify-between relative overflow-hidden">
+            <div className="h-full p-5 flex flex-col justify-between relative">
               <Character character={character} />
               <Loading difficulty={difficulty} character={character} />
             </div>
           )}
           {state > 4 && (
-            <div className="h-full p-5 flex flex-col justify-evenly items-center relative overflow-hidden">
+            <div className="h-full p-5 flex flex-col justify-evenly items-center relative">
               <Character character={character} state={state} />
               <Result
                 state={state}
